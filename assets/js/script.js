399357becef0afd49e9ace3f37092d17e93f6ced
Javier Calvo <jcalvo86@gmail.com>
2020-05-06 19:39:05 -0400

    diseño css small

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 $('.carousel').carousel()
+
+$(function() {
+    $("a").click(function(event) {
+        if (this.has !== "") {
+            event.preventDefault();
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function() {
+                window.location.hash = gato;
+            });
+        }
+    });
+});
